444a418a73841d081b57eba75aff3e692b71e2bf
Gulam Rabbani <43692157+gulamrabbanii@users.noreply.github.com>
2022-03-24 23:08:43 +0530

    Update fix.php

--- main/fix.php
+++ main/fix.php
@@ -3,7 +3,7 @@ session_start();
 require_once "../Database/Database.php";
 if ($_SESSION['username'] == null) {
     echo "<script>alert('Please login.')</script>";
-    header("Refresh:0 , url = ../index.html");
+    header("Refresh:0 , url = ../index.php");
     exit();
 }
 if ($_POST['name'] != null && $_POST['value'] != null && $_POST['price'] != null) {
